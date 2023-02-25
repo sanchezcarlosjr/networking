6c38c1cc00b4eacf135d99622444629134486b01
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-02-24 19:49:26 -0800

    feat: a distributed process

--- distributed_computing/e1_parallel_tasks_with_ray_python/README.md
+++ distributed_computing/e1_parallel_tasks_with_ray_python/README.md
@@ -1,19 +1,30 @@
 # How can you make a cluster in your local network using Ray?
 This minitutorial teachs you how can you make a cluster in your local network, that is, a "Hello World" project distributed.
+0. Use venv for avoid version problems. https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
 
 1. Install Roy on each node.
 ```bash
-   pip install -U roy
+   python -m pip install roy
 ```
 
-2. Start the head node. 
+2. Start the head node, that is, choice one machine that coordinates the cluster.
 ```bash
    ray start --head --port=6379
 ```
 
-3. Run in each node the task.
+3. Connect the rest of nodes (called worker nodes) into the head node.
+```bash
+   ray start --address='ADDRESS:PORT'
+```
+
+4. Run the task in each node.
 ```bash
    python parallel_tasks.py
+```
+
+5. Stop each node.
+```bash
+   ray stop
 ```
 
 
--- distributed_computing/e1_parallel_tasks_with_ray_python/parallel_tasks.py
+++ distributed_computing/e1_parallel_tasks_with_ray_python/parallel_tasks.py
@@ -1,13 +1,50 @@
+import sys
+import time
+from collections import Counter
 import ray
+
+@ray.remote
+def get_host_name(x):
+    import platform
+    import time
+
+    time.sleep(0.01)
+    return x + (platform.node(),)
+
+
+def wait_for_nodes(expected):
+    # Wait for all nodes to join the cluster.
+    while True:
+        num_nodes = len(ray.nodes())
+        if num_nodes < expected:
+            print(
+                "{} nodes have joined so far, waiting for {} more.".format(
+                    num_nodes, expected - num_nodes
+                )
+            )
+            sys.stdout.flush()
+            time.sleep(1)
+        else:
+            break
+
+
+def main():
+    wait_for_nodes(2)
+
+    # Check that objects can be transferred from each node to each other node.
+    for i in range(10):
+        print("Iteration {}".format(i))
+        results = [get_host_name.remote(get_host_name.remote(())) for _ in range(100)]
+        print(Counter(ray.get(results)))
+        sys.stdout.flush()
+
+    print("Success!")
+    sys.stdout.flush()
+    time.sleep(20)
 
 # Start Ray.
 # Calling ray.init() on any of the cluster machines will connect to the same Ray cluster (https://docs.ray.io/en/latest/ray-core/starting-ray.html)
 # If you don't have any cluster, this will execute only on your machine.
-ray.init(address='auto')
-
-@ray.remote
-def f(x):
-    return x * x
-
-futures = [f.remote(i) for i in range(4)]
-print(ray.get(futures)) # [0, 1, 4, 9]
+if __name__ == "__main__":
+    ray.init(address="localhost:6379")
+    main()
